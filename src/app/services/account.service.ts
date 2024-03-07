c203f12ed5e95ebd0e242ca6680f22add8c5de48
GerardGhub <singian28@gmail.com>
2024-03-08 01:14:58 +0800

    Login UI

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/register-user';
 import { Observable } from 'rxjs';
+import { LoginUser } from '../models/login-user';
 
 const API_BASE_URL: string = "https://localhost:7221/api/v1/account/";
 
@@ -10,14 +11,28 @@ const API_BASE_URL: string = "https://localhost:7221/api/v1/account/";
   providedIn: 'root'
 })
 export class AccountService {
+  public currentUserName: string | null = null;
 
   constructor(private httpClient: HttpClient) { }
 
 
   public postRegister(registerUser: RegisterUser): Observable<RegisterUser> {
     return this.httpClient.post<RegisterUser>(`${API_BASE_URL}register`, registerUser
-    // , { headers: headers }
-    )
+      // , { headers: headers }
+    );
   }
+
+  public postLogin(loginUser: LoginUser): Observable<LoginUser> {
+    return this.httpClient.post<LoginUser>(`${API_BASE_URL}login`, loginUser
+      // , { headers: headers }
+    );
+  }
+
+  public getLogout(): Observable<string> {
+    return this.httpClient.get<string>(`${API_BASE_URL}logout`
+      // , { headers: headers }
+    );
+  }
+
 
 }
