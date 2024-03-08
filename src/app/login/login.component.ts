b94372c27c3601e0b9bea0c72bfada121a3db5aa
GerardGhub <singian28@gmail.com>
2024-03-08 15:21:17 +0800

    Generating token adj

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -42,11 +42,12 @@ export class LoginComponent {
 
       this.accountService.postLogin
         (this.loginForm.value).subscribe({
-          next: (response: LoginUser) => {
+          next: (response: any) => {
             console.log(response);
 
             this.isLoginFormSubmitted = false;
             this.accountService.currentUserName = response.email;
+           localStorage["token"] = response.token;
             this.router.navigate(['/cities']);
             this.loginForm.reset();
           },
